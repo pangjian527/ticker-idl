90d48bbe1f678957c614dc965f8cd0321bb7fe4d
pangjian <pangjian@91htw.com>
2016-12-07 17:01:57 +0800

    add prdouct

---
+++ product.thrift
@@ -0,0 +1,37 @@
+namespace java com.tl.rpc.recharge
+
+include "common/rpc_error.thrift"
+include "common/rpc_security.thrift"
+
+struct SearchProductResult{
+    1:i32 totalCount,
+    2:list<Product> result,
+}
+
+enum PRODUCT_STATUS{
+    SALE=0,
+    OPEN=1,
+}
+
+struct Product{
+    1:string id,
+    2:string expect,
+    3:i64 balance,
+    4:i64 createTime,
+    5:i32 year,
+    6:i32 stage,
+    7:i64 updateTime,
+    8:PRODUCT_STATUS status
+}
+
+service ProductService{
+    /*发布信息*/
+    void saveProduct(1:rpc_security.ServiceToken accessToken,2:Product product)  throws (1:rpc_error.RpcException err);
+
+    /*查询*/
+    SearchProductResult searchProduct(1:rpc_security.ServiceToken accessToken,2:i32 limit,3:i32 offset)throws (1:rpc_error.RpcException err);
+
+    Product getByProductId(1:rpc_security.ServiceToken accessToken,2:string productId)  throws (1:rpc_error.RpcException err);
+
+
+}
